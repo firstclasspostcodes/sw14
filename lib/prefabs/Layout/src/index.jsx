84215c2492daa20ce58871d79a52279f0e41e6dc
davidkelley <david.james.kelley@gmail.com>
2020-02-06 16:10:38 +0000

    fix: 🐛 removing <ThemeProvider/> from Layout prefab

--- lib/prefabs/Layout/src/index.jsx
+++ lib/prefabs/Layout/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import { ThemeProvider } from '../../../components/ThemeProvider';
 import { Baseline } from '../../../components/Baseline';
 import { Pane } from '../../../components/Pane';
 import { Header } from '../../Header';
@@ -14,24 +13,19 @@ const Content = styled.div`
 
 Content.displayName = 'Layout.Content';
 
-export const Layout = ({ headerProps, footerProps, theme, children }) => (
-  <ThemeProvider theme={theme}>
+export const Layout = ({ headerProps, footerProps, children }) => (
+  <>
     <Baseline />
     <Header {...headerProps} skipLinkHref="#main-content-area" />
     <Content id="main-content-area">
       <Pane.Constrained spacing={{ px: 2 }}>{children}</Pane.Constrained>
     </Content>
     <Footer {...footerProps} />
-  </ThemeProvider>
+  </>
 );
 
 Layout.propTypes = {
-  theme: PropTypes.shape,
   headerProps: PropTypes.shape(Header.propTypes).isRequired,
   footerProps: PropTypes.shape(Footer.propTypes).isRequired,
   children: PropTypes.node.isRequired,
-};
-
-Layout.defaultProps = {
-  theme: {},
 };
